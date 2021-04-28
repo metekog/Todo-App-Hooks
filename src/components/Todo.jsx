8cfcc0f0ffc5b887f61887a98d6d3cc857e013d3
metekog <metekog@gmail.com>
2021-04-28 08:25:13 +0300

    .

---
+++ src/components/Todo.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useReducer, useState } from "react";
+import { ACTIONS } from "./actionTypes";
+import "../App.css";
+
+const reducer = (todos, action) => {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, addNewTodo(action.payload.name)];
+
+    case ACTIONS.COMPLETE_TODO:
+      return todos.map((todo) => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: !todo.complete };
+        } else {
+          return { ...todo };
+        }
+      });
+
+    case ACTIONS.DELETE_TODO:
+      return todos.filter((todo) => todo.id !== action.id);
+
+    default:
+      return todos;
+  }
+};
+
+const addNewTodo = (name) => {
+  return { id: Date.now(), name, complete: false };
+};
+
+export const Todo = () => {
+  const [name, setName] = useState("");
+
+  const [todos, dispatch] = useReducer(reducer, [], () => {
+    const localData = localStorage.getItem("todos");
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name } });
+    setName("");
+  };
+
+  return (
+    <div className="site-container">
+      <h1>TO-DO LIST</h1>
+      <form className="todo-form" onSubmit={handleSubmit}>
+        <input
+          className="todo-input"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="To Do"
+          required
+        />
+        <button className="todo-button">ADD</button>
+      </form>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <div
+            style={{
+              margin: "auto",
+              borderRadius: "10px",
+              width: "75%",
+              textDecoration: todo.complete ? "line-through" : "",
+              background: todo.complete ? "#23d41d" : "#e74242",
+            }}
+          >
+            <div className="todo-list">{todo.name}</div>
+          </div>
+          <div>
+            <button
+              onClick={() =>
+                dispatch({ type: ACTIONS.COMPLETE_TODO, id: todo.id })
+              }
+            >
+              {todo.complete ? "Not Completed" : "Completed"}
+            </button>
+            <button
+              onClick={() =>
+                dispatch({ type: ACTIONS.DELETE_TODO, id: todo.id })
+              }
+            >
+              Delete
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
